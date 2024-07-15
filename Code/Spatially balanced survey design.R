0bd52fb0f6e3e1d3e99c1f8b390414d13f6ab618
Louw-C <74646083+Louw-C@users.noreply.github.com>
2024-07-15 15:51:01 +0900

    Started with code. Need to figure out how to get a ASCII file from the project area shapefile.

--- Code/Spatially balanced survey design.R
+++ Code/Spatially balanced survey design.R
@@ -1,1 +1,5 @@
 #Script for a spatially balanced survey design
+#Load required packages
+library(MBHdesign) #For spatial sampling
+library(fields) #for lots of things, but for plotting in this example
+library(sp) #for reading the ascii file of cropped depths for the MPA
